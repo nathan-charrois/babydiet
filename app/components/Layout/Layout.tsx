8d9787f5bb016584f9d2a9b7f34b844cb08615e5
Nathan Charrois <nathancharrois@gmail.com>
2026-04-11 19:42:52 -0700

    Fix move analytics script to layout

--- app/components/Layout/Layout.tsx
+++ app/components/Layout/Layout.tsx
@@ -21,6 +21,7 @@ function Layout({ children }: LayoutProps) {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Dongle:wght@300;400;700&display=swap" rel="stylesheet" />
         <link rel="icon" type="image/png" sizes="192x192" href="/images/favicon.png"></link>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-6D4TE41TLR"></script>
       </head>
       <body>
         {children}
--- app/routes/Diet/components/DietAnalyticsProvider.tsx
+++ app/routes/Diet/components/DietAnalyticsProvider.tsx
@@ -13,11 +13,6 @@ declare global {
 function initGA() {
   if (window.gtag) return
 
-  const script = document.createElement('script')
-  script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`
-  script.async = true
-  document.head.appendChild(script)
-
   window.dataLayer = window.dataLayer || []
 
   function gtag(...args: unknown[]) {
